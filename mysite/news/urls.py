6c76d6a315ee66123a085e10cc1c510778fd7c6a
Blayne <roman.u.marchenko@gmail.com>
2021-03-22 21:02:57 +0200

    CBV second part

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -6,6 +6,6 @@ app_name = 'news'
 urlpatterns = [
     path('', views.HomeNews.as_view(), name='home'),
     path('category/<int:category_id>/', views.NewsByCategory.as_view(), name='category'),
-    path('news/<int:news_id>/', views.view_news, name='view_news'),
-    path('news/add-news/', views.add_news, name='add_news'),
+    path('news/<int:news_id>/', views.ViewNews.as_view(), name='view_news'),
+    path('news/add-news/', views.AddNews.as_view(), name='add_news'),
 ]
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 from django.http import HttpResponse
 from .models import News, Category
 from .forms import NewsForm
@@ -7,7 +8,7 @@ from .forms import NewsForm
 
 class HomeNews(ListView):
     model = News
-    template_name = 'news/category.html'
+    template_name = 'news/index.html'
     context_object_name = 'news'
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -21,7 +22,7 @@ class HomeNews(ListView):
 
 class NewsByCategory(ListView):
     model = News
-    template_name = 'news/index.html'
+    template_name = 'news/category.html'
     context_object_name = 'news'
     allow_empty = False
 
@@ -34,26 +35,26 @@ class NewsByCategory(ListView):
         return context
 
 
-def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    category = get_object_or_404(Category, pk=category_id)
-    return render(request, 'news/category.html', {'news': news,
-                                                  'category': category})
+class ViewNews(DetailView):
+    model = News
+    context_object_name = 'news_item'
+    pk_url_kwarg = 'news_id'
+    template_name = 'news/view_news.html'
 
 
-def view_news(request, news_id):
-    news_item = get_object_or_404(News, pk=news_id)
-    return render(request, 'news/view_news.html', {'news_item': news_item,
-                                                   })
+class AddNews(CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    success_url = reverse_lazy('news:home')
 
 
-def add_news(request):
-    if request.method == 'POST':
-        form = NewsForm(request.POST)
-        if form.is_valid():
-            news = form.save()
-            return redirect(news)
-
-    else:
-        form = NewsForm()
-    return render(request, 'news/add_news.html', {'form': form})
+# def add_news(request):
+#     if request.method == 'POST':
+#         form = NewsForm(request.POST)
+#         if form.is_valid():
+#             news = form.save()
+#             return redirect(news)
+#
+#     else:
+#         form = NewsForm()
+#     return render(request, 'news/add_news.html', {'form': form})
