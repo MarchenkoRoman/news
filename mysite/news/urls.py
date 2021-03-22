524533f63f7ca4e873b48c2a816d87aea491e953
Blayne <roman.u.marchenko@gmail.com>
2021-03-22 19:39:54 +0200

    CBV first part

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'news'
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('category/<int:category_id>/', views.get_category, name='category'),
+    path('', views.HomeNews.as_view(), name='home'),
+    path('category/<int:category_id>/', views.NewsByCategory.as_view(), name='category'),
     path('news/<int:news_id>/', views.view_news, name='view_news'),
     path('news/add-news/', views.add_news, name='add_news'),
 ]
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView
 from django.http import HttpResponse
 from .models import News, Category
 from .forms import NewsForm
 
 
-def index(request):
-    news = News.objects.all()
-    return render(request, 'news/index.html', {'news': news,
-                                               'title': 'News list'})
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/category.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(HomeNews, self).get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)
+
+
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/index.html'
+    context_object_name = 'news'
+    allow_empty = False
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['category_id'] ,is_published=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(NewsByCategory, self).get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
 
 
 def get_category(request, category_id):
