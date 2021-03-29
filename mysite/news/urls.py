b2546ace151d7ca1551da22e63d14b44e483b23d
Blayne <roman.u.marchenko@gmail.com>
2021-03-29 12:38:19 +0300

    add paginator customizate from bootsrap

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -5,6 +5,7 @@ app_name = 'news'
 
 urlpatterns = [
     path('', views.HomeNews.as_view(), name='home'),
+    path('test/', views.test, name='test'),
     path('category/<int:category_id>/', views.NewsByCategory.as_view(), name='category'),
     path('news/<int:news_id>/', views.ViewNews.as_view(), name='view_news'),
     path('news/add-news/', views.AddNews.as_view(), name='add_news'),
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 from django.http import HttpResponse
@@ -6,11 +8,20 @@ from .models import News, Category
 from .forms import NewsForm
 
 
+def test(request):
+    objects = ['john1', 'paul2', 'george3', 'ringo4', 'john5', 'paul6', 'george7']
+    paginator = Paginator(objects, 2)
+    page_num = request.GET.get('page', 1)
+    page_objects = paginator.get_page(page_num)
+    return render(request, 'news/test.html', {'page_obj': page_objects})
+
+
 class HomeNews(ListView):
     model = News
     template_name = 'news/index.html'
     context_object_name = 'news'
-    queryset = News.objects.select_related('category')
+    queryset = News.objects.filter(is_published=True).select_related('category')
+    paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(HomeNews, self).get_context_data(**kwargs)
@@ -26,6 +37,7 @@ class NewsByCategory(ListView):
     template_name = 'news/category.html'
     context_object_name = 'news'
     allow_empty = False
+    paginate_by = 2
 
     def get_queryset(self):
         return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True).select_related('category')
